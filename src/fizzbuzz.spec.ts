d020767699abb6129e07ce0b8469f0b0a73578c5
Jesus Maria Escudero Justel <jmescudero@plainconcepts.com>
2022-10-28 12:51:37 +0200

    First iteration

--- src/fizzbuzz.spec.ts
+++ src/fizzbuzz.spec.ts
@@ -1,7 +1,156 @@
-import { hello } from "./fizzbuzz";
+import { run, runNumber } from "./fizzbuzz";
 
 describe("FizzBuzz", () => {
-  it("hello", () => {
-    expect(hello()).toEqual("hello");
+  it.only("Returns all", () => {
+    expect(run()).toEqual([
+      "1",
+      "2",
+      "Fizz",
+      "4",
+      "Buzz",
+      "Fizz",
+      "7",
+      "8",
+      "Fizz",
+      "Buzz",
+      "11",
+      "Fizz",
+      "13",
+      "14",
+      "FizzBuzz",
+      "16",
+      "17",
+      "Fizz",
+      "19",
+      "Buzz",
+      "Fizz",
+      "22",
+      "23",
+      "Fizz",
+      "Buzz",
+      "26",
+      "Fizz",
+      "28",
+      "29",
+      "FizzBuzz",
+      "31",
+      "32",
+      "Fizz",
+      "34",
+      "Buzz",
+      "Fizz",
+      "37",
+      "38",
+      "Fizz",
+      "Buzz",
+      "41",
+      "Fizz",
+      "43",
+      "44",
+      "FizzBuzz",
+      "46",
+      "47",
+      "Fizz",
+      "49",
+      "Buzz",
+      "Fizz",
+      "52",
+      "53",
+      "Fizz",
+      "Buzz",
+      "56",
+      "Fizz",
+      "58",
+      "59",
+      "FizzBuzz",
+      "61",
+      "62",
+      "Fizz",
+      "64",
+      "Buzz",
+      "Fizz",
+      "67",
+      "68",
+      "Fizz",
+      "Buzz",
+      "71",
+      "Fizz",
+      "73",
+      "74",
+      "FizzBuzz",
+      "76",
+      "77",
+      "Fizz",
+      "79",
+      "Buzz",
+      "Fizz",
+      "82",
+      "83",
+      "Fizz",
+      "Buzz",
+      "86",
+      "Fizz",
+      "88",
+      "89",
+      "FizzBuzz",
+      "91",
+      "92",
+      "Fizz",
+      "94",
+      "Buzz",
+      "Fizz",
+      "97",
+      "98",
+      "Fizz",
+      "Buzz",
+    ]);
   });
+
+  //describe(FizzBuzz, () => {
+  //
+  //  it(Returns Fizz for 30, () => {
+  //    expect(runNumber(30)).toEqual(FizzBuzz);
+  //  });
+  //  it(Returns FizzBuzzBuzz for 35, () => {
+  //    expect(runNumber(35)).toEqual(FizzBuzzBuzz);
+  //  });
+  //  it(Returns Buzz for 50, () => {
+  //    expect(runNumber(50)).toEqual(Buzz);
+  //  });
+  //  it(Returns Buzz for 55, () => {
+  //    expect(runNumber(55)).toEqual(Buzz);
+  //  });
+  //  it(Returns FizzBuzz for 53, () => {
+  //    expect(runNumber(53)).toEqual(FizzBuzz);
+  //  });
+  //});
 });
+
+/*
+Devuelve Fizz si el número es divisible por 3.
+Devuelve Buzz si el número es divisible por 5.
+Devuelve FizzBuzz si el número es divisible por 3 y por 5.
+
+1
+2
+Fizz
+4
+
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+... etc hasta el 100
+*/
--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,1 +1,48 @@
-export const hello = () => "hello";
+export const run = (): string[] => {
+  const numbers: string[] = [];
+
+  for (let i = 1; i <= 100; i++) {
+    const value = runNumber(i);
+    numbers.push(value);
+  }
+
+  return numbers;
+};
+
+export const runNumber = (aNumber: number): string => {
+  const fizz: string = "Fizz";
+  const buzz: string = "Buzz";
+  let value = "";
+
+  const strategies: {
+    order: number;
+    canHandle: (i: number, currentValue: string) => boolean;
+    execute: (i: number, currentValue: string) => string;
+  }[] = [
+    {
+      order: 1,
+      canHandle: (i, _) => i % 3 == 0,
+      execute: (_, currentValue) => currentValue + fizz,
+    },
+    {
+      order: 2,
+      canHandle: (i, _) => i % 5 == 0,
+      execute: (_, currentValue) => currentValue + buzz,
+    },
+    {
+      order: 3,
+      canHandle: (_, currentValue) => currentValue === "",
+      execute: (i, _) => i.toString(),
+    },
+  ];
+
+  strategies
+    .sort((a, b) => (a > b ? -1 : 1))
+    .forEach((strategy) => {
+      if (strategy.canHandle(aNumber, value)) {
+        value = strategy.execute(aNumber, value);
+      }
+    });
+
+  return value;
+};
