f83da5d61f6c41bced6f1065a2ec15d333090128
borja <bandres@plainconcepts.com>
2022-10-28 14:42:06 +0200

    resolved

--- src/fizzbuzz.spec.ts
+++ src/fizzbuzz.spec.ts
@@ -1,7 +1,41 @@
-import { hello } from "./fizzbuzz";
+import { hello, isFizzOrBuzz } from "./fizzbuzz";
 
 describe("FizzBuzz", () => {
   it("hello", () => {
     expect(hello()).toEqual("hello");
+  });
+});
+
+describe("if number is divisible by", () => {
+  it("3 then fizz", () => {
+    const dummies: number[] = [6, 9, 12, 18, 24, 27];
+
+    dummies.forEach((dummy) => {
+      expect(isFizzOrBuzz(dummy)).toEqual("fizz");
+    });
+  });
+
+  it("5 then buzz", () => {
+    const dummies: number[] = [5, 10, 20, 25, 35];
+
+    dummies.forEach((dummy) => {
+      expect(isFizzOrBuzz(dummy)).toEqual("buzz");
+    });
+  });
+
+  it("3 and 5 then fizzbuzz", () => {
+    const dummies: number[] = [15, 30, 90, 120, 150];
+
+    dummies.forEach((dummy) => {
+      expect(isFizzOrBuzz(dummy)).toEqual("fizzbuzz");
+    });
+  });
+
+  it("none then empty", () => {
+    const dummies: number[] = [2, 7, 11, 13, 17];
+
+    dummies.forEach((dummy) => {
+      expect(isFizzOrBuzz(dummy)).toEqual("");
+    });
   });
 });
--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,1 +1,25 @@
 export const hello = () => "hello";
+
+export const fizzbuzz = () => {
+  const values: string[] = [];
+
+  for (let i = 1; i <= 100; i++) {
+    const alias = isFizzOrBuzz(i);
+    values.push(alias + i);
+  }
+
+  return values;
+};
+
+export const isFizzOrBuzz = (numberToCheck: number): string => {
+  let alias = "";
+
+  if (numberToCheck % 3 === 0) {
+    alias = "fizz";
+  }
+
+  if (numberToCheck % 5 === 0) {
+    alias += "buzz";
+  }
+  return alias;
+};
